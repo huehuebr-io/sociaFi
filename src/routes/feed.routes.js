b1a7b821f8767cac69e1ab3e72f852f9c7c488be
Jorge Pereira <55901804+jorgep23@users.noreply.github.com>
2025-12-18 10:29:10 -0300

    Update feed.routes.js

--- src/routes/feed.routes.js
+++ src/routes/feed.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { db } from "../db.js";
 import { authMiddleware } from "../middleware/auth.js";
+import { authMiddlewareOptional } from "../middlewareOptional/auth.js";
 import { checkFounder } from "../services/founder.js";
 
 const router = express.Router();
